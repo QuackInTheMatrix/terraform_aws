702c878cab1d248666aa0154181c7c647fc61e52
quackinthematrix <therealpepega@protonmail.com>
2023-11-25 14:20:59 +0100

    Added new feature to index.html and fortune.js
    index.html
    	Updated identifiers and text inside of for the new feature.
    fortune.js
    	Modified the button listener to subscribe users to daily fortunes.
    	The modified listener now also provides email verification.
    	Note: The API endpoint is currently not available and will be updated after it is created.

--- Microservices/website/fortune.js
+++ Microservices/website/fortune.js
@@ -7,10 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(error => {
             console.error('Error fetching data:', error);
     });
-    document.getElementById("fortune-button").addEventListener("click", () => {
-        const fortuneInput = document.getElementById("fortune-input").value;
-        const dataToAdd = { "message": fortuneInput };
-        fetch('https://api.ivandeveric.site/api/db/fortunes', {
+    document.getElementById("subscribe-button").addEventListener("click", () => {
+        function validateEmail(email) {
+            return String(email)
+                .toLowerCase()
+                .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                );
+        }
+        const emailInput = document.getElementById("email-input").value;
+        if (!validateEmail(emailInput)) {
+            document.getElementById("confirm-subscribe").textContent=`Email "${emailInput}" is invalid!`;
+            return;
+        }else{
+            document.getElementById("confirm-subscribe").textContent="";
+        }
+        const dataToAdd = { "message": emailInput };
+        fetch('https://api.ivandeveric.site/TODO_ENDPOINT', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -19,10 +32,10 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(response => response.json())
         .then(data => {
-            document.getElementById("confirm-insert").textContent="Success!";
+            document.getElementById("confirm-subscribe").textContent="Success! Please check your email and confirm.";
         })
         .catch(error => {
-            document.getElementById("confirm-insert").textContent="Failure!";
+            document.getElementById("confirm-subscribe").textContent="Failure!";
             console.error('Error updating data:', error);
         });
     });
